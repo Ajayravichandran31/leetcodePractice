3a4b0dab3006ab4dd279f29989afec40deb64c0a
Ajay <ajayravichandran31@gmail.com>
2026-09-09 12:35:57 +0530

    solved MergeSortedArray

---
+++ MergeSortedArray.java
@@ -0,0 +1,29 @@
+public class MergeSortedArray {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int p1 = m - 1;
+        int p2 = n - 1;
+        int i = m + n - 1;
+
+        while (p1 >= 0 && p2 >= 0) {
+            if (nums1[p1] > nums2[p2]) {
+                nums1[i--] = nums1[p1--];
+            } else {
+                nums1[i--] = nums2[p2--];
+            }
+        }
+
+        while (p2 >= 0) {
+            nums1[i--] = nums2[p2--];
+        }
+    }
+
+    public static void main(String[] args) {
+        MergeSortedArray solution = new MergeSortedArray();
+        int[] nums1 = {1, 2, 3, 0, 0, 0};
+        int[] nums2 = {2, 5, 6};
+        solution.merge(nums1, 3, nums2, 3);
+        for (int num : nums1) {
+            System.out.print(num + " ");
+        }
+    }
+}
