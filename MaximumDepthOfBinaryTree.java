b022f371656c592f97f3983178fe13937317ae57
Ajay <ajayravichandran31@gmail.com>
2026-09-10 15:42:14 +0530

    Solved MaximumDepthOfBinaryTree problem

---
+++ MaximumDepthOfBinaryTree.java
@@ -0,0 +1,34 @@
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class MaximumDepthOfBinaryTree {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+        return 1 + Math.max(left, right);
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20, new TreeNode(15), new TreeNode(7));
+
+        MaximumDepthOfBinaryTree solution = new MaximumDepthOfBinaryTree();
+        System.out.println(solution.maxDepth(root));
+    }
+}
