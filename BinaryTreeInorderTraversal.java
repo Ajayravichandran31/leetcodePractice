147a87dcbb545f7bad927a74cf52ed985f52b962
Ajay <ajayravichandran31@gmail.com>
2026-09-10 13:46:10 +0530

    Solved BinaryTreeInorderTraversal problem

---
+++ BinaryTreeInorderTraversal.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class BinaryTreeInorderTraversal {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode curr = root;
+
+        while (curr != null || !stack.isEmpty()) {
+            while (curr != null) {
+                stack.push(curr);
+                curr = curr.left;
+            }
+
+            curr = stack.pop();
+            result.add(curr.val);
+            curr = curr.right;
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        BinaryTreeInorderTraversal solution = new BinaryTreeInorderTraversal();
+        TreeNode root = new TreeNode(1, null, new TreeNode(2, new TreeNode(3), null));
+        System.out.println(solution.inorderTraversal(root));
+    }
+}
