0dd87733709336b27445f2f6580e8623e55cc0ad
Ajay <ajayravichandran31@gmail.com>
2026-09-12 21:24:54 +0530

    Solved ConvertSortedArrayToBinarySearchTree problem

---
+++ ConvertSortedArrayToBinarySearchTree.java
@@ -0,0 +1,34 @@
+public class ConvertSortedArrayToBinarySearchTree {
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return buildTree(nums, 0, nums.length - 1);
+    }
+
+    private TreeNode buildTree(int[] nums, int left, int right) {
+        if (left > right) {
+            return null;
+        }
+
+        int mid = left + (right - left) / 2;
+        TreeNode node = new TreeNode(nums[mid]);
+
+        node.left = buildTree(nums, left, mid - 1);
+        node.right = buildTree(nums, mid + 1, right);
+
+        return node;
+    }
+
+    public static void main(String[] args) {
+        ConvertSortedArrayToBinarySearchTree solution = new ConvertSortedArrayToBinarySearchTree();
+        int[] nums = {-10, -3, 0, 5, 9};
+        TreeNode root = solution.sortedArrayToBST(nums);
+    }
+}
