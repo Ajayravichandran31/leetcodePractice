dce8b6888000269850703f8439d90d0792e61b82
Ajay <ajayravichandran31@gmail.com>
2026-09-16 22:07:26 +0530

    Solved BestTimeToBuyAndSellStock problem

---
+++ BestTimeToBuyAndSellStock.java
@@ -0,0 +1,24 @@
+public class BestTimeToBuyAndSellStock {
+    public int maxProfit(int[] prices) {
+        if (prices == null || prices.length == 0) {
+            return 0;
+        }
+        int minPrice = prices[0];
+        int maxProfit = 0;
+
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];
+            } else if (prices[i] - minPrice > maxProfit) {
+                maxProfit = prices[i] - minPrice;
+            }
+        }
+        return maxProfit;
+    }
+
+    public static void main(String[] args) {
+        BestTimeToBuyAndSellStock solution = new BestTimeToBuyAndSellStock();
+        int[] prices = {7, 1, 5, 3, 6, 4};
+        System.out.println(solution.maxProfit(prices));
+    }
+}
