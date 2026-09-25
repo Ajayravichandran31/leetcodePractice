4fba27342a4c87fb614b0aa40adbf2e90204bb5a
Ajay <ajayravichandran31@gmail.com>
2026-09-25 08:11:42 +0530

    Solved BraceExpansionII Problem

---
+++ BraceExpansionII.java
@@ -0,0 +1,58 @@
+class BraceExpansionII {
+    public List<String> braceExpansionII(String expression) {
+        Set<String> set = parse(expression);
+        List<String> list = new ArrayList<>(set);
+        Collections.sort(list);
+        return list;
+    }
+
+    private Set<String> parse(String expr) {
+        Set<String> res = new HashSet<>();
+        List<Set<String>> groups = new ArrayList<>();
+        Set<String> curGroup = new HashSet<>(Arrays.asList(""));
+
+        int i = 0;
+        while (i < expr.length()) {
+            char ch = expr.charAt(i);
+
+            if (ch == '{') {
+                int count = 1, start = ++i;
+                while (count > 0) {
+                    if (expr.charAt(i) == '{') {
+                        count++;
+                    } else if (expr.charAt(i) == '}') {
+                        count--;
+                    }
+                    i++;
+                }
+                Set<String> subRes = parse(expr.substring(start, i - 1));
+                curGroup = combine(curGroup, subRes);
+            } else if (ch == ',') {
+                groups.add(curGroup);
+                curGroup = new HashSet<>(Arrays.asList(""));
+                i++;
+            } else {
+                Set<String> single = new HashSet<>(Arrays.asList("" + ch));
+                curGroup = combine(curGroup, single);
+                i++;
+            }
+        }
+
+        groups.add(curGroup);
+        for (Set<String> g : groups) {
+            res.addAll(g);
+        }
+
+        return res;
+    }
+
+    private Set<String> combine(Set<String> s1, Set<String> s2) {
+        Set<String> res = new HashSet<>();
+        for (String str1 : s1) {
+            for (String str2 : s2) {
+                res.add(str1 + str2);
+            }
+        }
+        return res;
+    }
+}
